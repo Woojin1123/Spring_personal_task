c36a147dfd741a1aa7e03c4db1a7eded6d70c904
Woojin1123 <boom175839@gmail.com>
2024-08-12 13:56:46 +0900

    init comment

---
+++ src/main/java/com/nbc/springpersonaltask/SpringPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nbc.springpersonaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersonalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbc/springpersonaltask/SpringPersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbc.springpersonaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
